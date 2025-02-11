d537009ce13162b67e4a483f5d646ce208576015
Wahyu Nugroho Gultom <71364108+yumayko@users.noreply.github.com>
2025-02-11 21:09:26 +0700

    make form subscribe transaction admin side

--- app/Filament/Resources/SubscribeTransactionResource.php
+++ app/Filament/Resources/SubscribeTransactionResource.php
@@ -2,16 +2,18 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\SubscribeTransactionResource\Pages;
-use App\Filament\Resources\SubscribeTransactionResource\RelationManagers;
-use App\Models\SubscribeTransaction;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use App\Models\SubscribePackage;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Grid;
+use App\Models\SubscribeTransaction;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\SubscribeTransactionResource\Pages;
+use App\Filament\Resources\SubscribeTransactionResource\RelationManagers;
 
 class SubscribeTransactionResource extends Resource
 {
@@ -23,7 +25,78 @@ class SubscribeTransactionResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\Wizard::make([
+                    Forms\Components\Wizard\Step::make('Product and Price')
+                    ->schema([
+                        Grid::make(2)
+                        ->schema([
+                            Forms\Components\Select::make('subscribe_package_id')
+                            ->relationship('SubscribeTransaction', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required()
+                            ->live()
+                            ->afterStateUpdated(function ($state, callable $set){
+                                $subscribePackage = SubscribePackage::find($state);
+                                $price = $subscribePackage ? $subscribePackage->price : 0;
+                                $duration = $subscribePackage ? $subscribePackage->diration : 0;
+
+                                $set('price', $price);
+                                $set('diration', $duration);
+
+                                $tax = 0.11;
+                                $totalTaxAmount = $tax * $price;
+
+                                $totalAmount = $price + $totalTaxAmount;
+                                $set('total_amount', number_format($totalAmount, 0, '', ''));
+                                $set('total_tax_amount', number_format($totalTaxAmount, 0, '', ''));
+                            })
+
+                            ->afterStateHydrated(function (callable $get, callable $set, $state){
+                                $subscribePackageId = $state;
+                                if($subscribePackageId){
+                                    $subscribePackage = SubscribePackage::find($subscribePackageId);
+                                    $price = $subscribePackage ? $subscribePackage->price : 0;
+                                    $set('price', $price);
+
+                                    $tax = 0.11;
+                                    $totalTaxAmount = $tax * $price;
+                                    $set('total_tax_amount', number_format($totalTaxAmount, 0, '', ''));
+                                }
+                            }),
+
+                            Forms\Components\TextInput::make('price')
+                            ->required()
+                            ->readOnly()
+                            ->numeric()
+                            ->prefix('IDR'),
+
+                            Forms\Components\TextInput::make('total_amount')
+                            ->required()
+                            ->readOnly()
+                            ->numeric()
+                            ->prefix('IDR'),
+
+                            Forms\Components\TextInput::make('total_tax_amount')
+                            ->readOnly()
+                            ->required()
+                            ->numeric()
+                            ->prefix('IDR'),
+
+                            Forms\Components\DatePicker::make('started_at')
+                            ->required(),
+
+                            Forms\Components\DatePicker::make('ended_at')
+                            ->required(),
+
+                            Forms\Components\TextInput::make('duration')
+                            ->required()
+                            ->readOnly()
+                            ->numeric()
+                            ->prefix('Days'),
+                        ]),
+                    ])
+                ])
             ]);
     }
 
